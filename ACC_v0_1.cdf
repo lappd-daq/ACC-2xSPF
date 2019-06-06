ed522fa428a928b71aee1dc1e5860faa509545a4
Evan Angelico <ejangelico@uchicago.edu>
2019-06-06 18:45:45 -0500

    external SMA trigger
    This commit, I fixed the logic that latches on the SMA external trigger. To use this trigger method, in software you should set the configuration "wait for sys" using the 0x7 trigger settings lo command. This was tested in conjunction with a self trigger on the acdc. How this trigger method works when varying the options in the 0x7 "hi" block is untested (for example, varying how long the ACDC should hold the event for)

--- ACC_v0_1.cdf
+++ ACC_v0_1.cdf
@@ -1,10 +1,10 @@
-/* Quartus Prime Version 15.1.0 Build 185 10/21/2015 SJ Standard Edition */
+/* Quartus Prime Version 16.0.0 Build 211 04/27/2016 SJ Standard Edition */
 JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(5AGXFB5H4F35) Path("C:/Users/eric/Desktop/ACC-2xSFP-firmware/") File("ACC_v0_1.sof") MfrSpec(OpMask(1));
+		Device PartName(5AGXFB5H4F35) Path("C:/Users/ejangelico/Documents/GitHub/ACC-2xSPF/") File("ACC_v0_1.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
--- src/usbWrapperACC.vhd
+++ src/usbWrapperACC.vhd
@@ -81,8 +81,8 @@ entity usbWrapperACC is
 		xWAKEUP_USB				:  out   std_logic;
 		xCC_SYNC_OUT			:  out	std_logic;
 		xTRIG_VALID				: 	out	std_logic;
-	   xSOFT_TRIG_BIN			:  out	std_logic_vector(2 downto 0));
-
+	   xSOFT_TRIG_BIN			:  out	std_logic_vector(2 downto 0);
+		xWAIT_FOR_SYS 			: 	out	std_logic);
 		
 end usbWrapperACC;
 
@@ -138,6 +138,7 @@ architecture behavioral of usbWrapperACC is
 	signal RESET_DLL_FLAG	: std_logic;
 	signal HARD_RESET       : std_logic := '0';
 	signal WAKEUP_USB 		: std_logic := '0';
+	signal WAIT_FOR_SYS		: std_logic := '0';
 		
 	--USB_instructions: TMP, GOOD => for syncing to master clock
 	signal trig_valid					: std_logic;
@@ -223,6 +224,7 @@ architecture behavioral of usbWrapperACC is
 	xHARD_RESET       <= HARD_RESET;
 	xCC_SYNC_OUT		<= CC_SYNC_REG;
 	xTRIG_VALID			<= trig_valid;
+	xWAIT_FOR_SYS 		<= WAIT_FOR_SYS;
 	
 	--reset_from_usb		<= xCLR_ALL;
 	
@@ -916,6 +918,15 @@ end process;
 						when others =>
 							CC_INSTRUCTION <= USB_INSTRUCTION;
 							CC_INSTRUCT_RDY<= '1';
+							
+							--helps to know if the ACDC is waiting for 
+							--a sys signal, this gets passed over to triggerAndTime.
+							--(15 downto 12 flags if this is the lo vs hi cmd. wait for sys
+							-- comes on lo command)
+							if USB_INSTRUCTION(19 downto 16) = x"7" and USB_INSTRUCTION(15 downto 12) = x"0" then
+								WAIT_FOR_SYS <= USB_INSTRUCTION(1);
+							end if;
+								
 							if delay > 8 then
 								delay := 0;
 								state <= st1_WAIT;
